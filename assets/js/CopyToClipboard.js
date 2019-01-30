1307d59edc421d10e66de2735a2f9da451673595
Krzysztof Owsiany <gocom@o2.pl>
2019-01-30 12:16:52 +0100

    Add js with copy to clipboard

---
+++ assets/js/CopyToClipboard.js
@@ -0,0 +1,13 @@
+function CopyToClipboard(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+  
+  //<a href="javascript:CopyToClipboard('adsfasdfasdfsdafasdfdsfsadfsad')">💃</a>
